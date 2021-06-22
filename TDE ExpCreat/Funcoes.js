4fb2499f8cd8d7c2f9fa591e35f8cbb1961cb02d
EnzoFav <80775075+EnzoFav@users.noreply.github.com>
2021-06-21 21:27:15 -0300

    commit inicial

---
+++ TDE ExpCreat/Funcoes.js
@@ -0,0 +1,25 @@
+function ExpCreat(){
+
+    window.location.href = "ExpCreat.html";
+
+}
+
+function Descoberta(){
+
+    window.location.href = "Descoberta.html";
+    
+}
+
+function RacAlg(){
+
+    window.location.href = "RacAlg.html";
+    
+}
+
+
+
+function Menu(){
+
+    window.location.href = "Menu.html";
+
+}
